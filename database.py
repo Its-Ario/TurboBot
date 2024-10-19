c2f37afd347f95b3c299043b839dd2120d6c318d
Ario Shakeri <itsario900@gmail.com>
2024-10-19 16:45:54 +0330

    Fixed other database functions

--- database.py
+++ database.py
@@ -9,14 +9,14 @@ def create_database():
     conn.commit()
     conn.close()
 def read_database():
-    conn = sqlite3.connect('database.db')
+    conn = sqlite3.connect('Data/database.db')
     cursor = conn.cursor()
     cursor.execute("SELECT key, value FROM key_value_store")
     rows = cursor.fetchall()
     conn.close()
     return {key: json.loads(value) for key, value in rows}
 def write_database(data_dict):
-    conn = sqlite3.connect('database.db')
+    conn = sqlite3.connect('Data/database.db')
     cursor = conn.cursor()
     for key, value in data_dict.items():
         cursor.execute("INSERT OR REPLACE INTO key_value_store (key, value) VALUES (?, ?)",
